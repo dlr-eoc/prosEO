6cea7e359fc21d0812bc472c5275fd80565fc53a
Thomas Bassler <thomas.bassler@drbassler.de>
2019-05-01 09:56:23 +0200

    Creation of "proseo-model" module

---
+++ model/src/main/java/de/dlr/proseo/model/PersistentObject.java
@@ -0,0 +1,110 @@
+/**
+ * PersistentObject.java
+ * 
+ * (C) 2019 Dr. Bassler & Co. Managementberatung GmbH
+ */
+package de.dlr.proseo.model;
+
+import java.util.Objects;
+
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Abstract superclass of all persistent classes
+ * 
+ * @author Thomas Bassler
+ */
+@MappedSuperclass
+@Inheritance(strategy=InheritanceType.JOINED)
+public class PersistentObject {
+	/** Next object id for assignment */
+	private static long nextId = System.currentTimeMillis(); // Seeded by the current time
+
+	/**
+	 * The persistent id of this object (an "assigned identifier" according to JPA).
+	 */
+	@Id
+	private long id;
+	
+	/**
+	 * The modification version of this object
+	 */
+	private int version;
+	
+	/**
+	 * Get the next available object id
+	 * @return a unique object id
+	 */
+	private static synchronized long getNextId() {
+		return ++nextId;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
+
+	/**
+	 * Test equality of persistent objects based on their unique ID.
+	 * 
+	 * @param obj the object to compare this object to
+	 * @return true, if obj is a persistent object and has the same ID, false otherwise
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof PersistentObject))
+			return false;
+		PersistentObject other = (PersistentObject) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	/**
+	 * Set the id of the persistent object.
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the id of the persistent object
+	 * @return the object id
+	 */
+	public long getId() {
+		return this.id;
+	}
+
+	/**
+	 * Gets the version of the persistent object
+	 * @return the object version
+	 */
+	public int getVersion() {
+		return version;
+	}
+
+	/**
+	 * Increments the version of the persistent object
+	 */
+	public void incrementVersion() {
+		this.version++;
+	}
+
+	/**
+	 * No-argument constructor that assigns the object id
+	 */
+	public PersistentObject() {
+		super();
+		id = getNextId();
+	}
+
+}
---
+++ model/src/main/java/de/dlr/proseo/model/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * package-info.java
+ * 
+ * (C) 2019 Dr. Bassler & Co. Managementberatung GmbH
+ */
+/**
+ * The de.dlr.proseo.model package contains all classes required for the modelling of the persistent state
+ * of prosEO, the Processing System for Earth Observation Data
+ * 
+ * @author Dr. Thomas Bassler
+ *
+ */
+package de.dlr.proseo.model;
