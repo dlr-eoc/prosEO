218c0521e4e67b717efe548ffff385965a52bd40
Thomas Bassler <thomas.bassler@drbassler.de>
2019-05-08 19:07:04 +0200

    Initial development of sample processor

---
+++ sample-processor/src/main/java/de/dlr/proseo/sampleproc/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * package-info.java
+ */
+/**
+ * prosEO Sample Processor package containing a minimal dummy processor conforming to ESA's
+ * "Generic IPF Interface Specification" (MMFI-GSEG-EOPG-TN-07-0003, V.1.8)
+ * 
+ * @author Dr. Thomas Bassler
+ *
+ */
+package de.dlr.proseo.sampleproc;
