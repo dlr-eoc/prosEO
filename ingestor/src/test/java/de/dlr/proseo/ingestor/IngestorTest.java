914a6cd47919ae3bf20141aebda9841924cefa13
Thomas Bassler <thomas.bassler@drbassler.de>
2019-05-15 18:53:15 +0200

    Migration to Java 11 and Springboot 2.1.4 completed

--- ingestor/src/test/java/de/dlr/proseo/ingestor/IngestorTest.java
+++ ingestor/src/test/java/de/dlr/proseo/ingestor/IngestorTest.java
@@ -15,9 +15,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.*;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -27,8 +28,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import de.dlr.proseo.ingestor.rest.model.IngestorProduct;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -41,16 +40,14 @@ import static org.junit.Assert.assertTrue;
  * @author Dr. Thomas Bassler
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Ingestor.class)
-@WebAppConfiguration
-@IntegrationTest("server.port=0")
+@SpringBootTest(classes = Ingestor.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 public class IngestorTest {
 
 	@Autowired
 	private SecurityProperties security;
 
-	@Value("${local.server.port}")
+	@LocalServerPort
 	private int port;
 
 	@Test
@@ -84,38 +81,18 @@ public class IngestorTest {
 	}
 
 	@Test
-	public void testMetrics() throws Exception {
-		// Makes sure a request has been made
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<List> dummyEntity = new TestRestTemplate("user", getPassword())
-				.getForEntity("http://localhost:" + this.port + "/products", List.class);
-		assertEquals(HttpStatus.OK, dummyEntity.getStatusCode());
-
-		// Now check request metrics
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = new TestRestTemplate("user", getPassword())
-				.getForEntity("http://localhost:" + this.port + "/metrics", Map.class);
+	public void testInfo() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate("user", getPassword())
+				.getForEntity("http://localhost:" + this.port + "/actuator/info", String.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		@SuppressWarnings("unchecked")
-		Map<String, Object> body = entity.getBody();
-		assertTrue("Wrong body: " + body, body.containsKey("counter.status.200.products"));
-	}
-
-	@Test
-	public void testEnv() throws Exception {
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = new TestRestTemplate("user", getPassword())
-				.getForEntity("http://localhost:" + this.port + "/env", Map.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		@SuppressWarnings("unchecked")
-		Map<String, Object> body = entity.getBody();
-		assertTrue("Wrong body: " + body, body.containsKey("systemProperties"));
+		assertTrue("Wrong body: " + entity.getBody(),
+				entity.getBody().equals("{}"));
 	}
 
 	@Test
 	public void testHealth() throws Exception {
 		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
-				"http://localhost:" + this.port + "/health", String.class);
+				"http://localhost:" + this.port + "/actuator/health", String.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
 		assertTrue("Wrong body: " + entity.getBody(),
 				entity.getBody().contains("\"status\":\"UP\""));
@@ -144,48 +121,6 @@ public class IngestorTest {
 		assertNotNull("Body was null", body);
 		assertTrue("Wrong body: " + body,
 				body.contains("This application has no explicit mapping for /error"));
-	}
-
-	@Test
-	public void testTrace() throws Exception {
-		new TestRestTemplate().getForEntity("http://localhost:" + this.port + "/health",
-				String.class);
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<List> entity = new TestRestTemplate("user", getPassword())
-				.getForEntity("http://localhost:" + this.port + "/trace", List.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		@SuppressWarnings("unchecked")
-		List<Map<String, Object>> list = entity.getBody();
-		Map<String, Object> trace = list.get(list.size() - 1);
-		@SuppressWarnings("unchecked")
-		Map<String, Object> map = (Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) trace
-				.get("info")).get("headers")).get("response");
-		assertEquals("200", map.get("status"));
-	}
-
-	@Test
-	public void testErrorPageDirectAccess() throws Exception {
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
-				"http://localhost:" + this.port + "/error", Map.class);
-		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, entity.getStatusCode());
-		@SuppressWarnings("unchecked")
-		Map<String, Object> body = entity.getBody();
-		assertEquals("None", body.get("error"));
-		assertEquals(999, body.get("status"));
-	}
-
-	@Test
-	public void testBeans() throws Exception {
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<List> entity = new TestRestTemplate("user", getPassword())
-				.getForEntity("http://localhost:" + this.port + "/beans", List.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		assertEquals(1, entity.getBody().size());
-		@SuppressWarnings("unchecked")
-		Map<String, Object> body = (Map<String, Object>) entity.getBody().get(0);
-		assertTrue("Wrong body: " + body,
-				((String) body.get("context")).startsWith("application"));
 	}
 
 	private String getPassword() {
