b6723dfa73dcb268e254f538773f77af49d8d43c
Hubert Asamer <hubert.asamer@dlr.de>
2019-05-22 17:50:02 +0200

    Extended sample-wrapper incl. tests (JOF-Handling, Container-Env); now ready for file i/o - implementation.

--- sample-wrapper/src/main/java/de/dlr/proseo/samplewrap/SampleWrapper.java
+++ sample-wrapper/src/main/java/de/dlr/proseo/samplewrap/SampleWrapper.java
@@ -2,41 +2,38 @@
  * SampleWrapper.java
  * 
  * (C) 2019 Dr. Bassler & Co. Managementberatung GmbH
+ * (C) 2019 Hubert Asamer
  */
 package de.dlr.proseo.samplewrap;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.CompleteMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.model.CompletedMultipartUpload;
-import software.amazon.awssdk.services.s3.model.CompletedPart;
-import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadResponse;
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.s3.model.UploadPartRequest;
-import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
-import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 
 /**
@@ -44,6 +41,7 @@ import software.amazon.awssdk.core.sync.ResponseTransformer;
  * "Generic IPF Interface Specification" (MMFI-GSEG-EOPG-TN-07-0003, V.1.8)
  * 
  * @author Dr. Thomas Bassler
+ * @author Hubert Asamer
  *
  */
 public class SampleWrapper {
@@ -57,34 +55,146 @@ public class SampleWrapper {
 	/** Exit code explanation for failure */
 	private static final String EXIT_TEXT_FAILURE = "FAILURE";
 	
-	// Error messages
+	/** Error messages */
 	private static final String MSG_LEAVING_SAMPLE_WRAPPER = "Leaving sample-wrapper with exit code {} ({})";
 	private static final String MSG_STARTING_SAMPLE_WRAPPER = "Starting sample-wrapper V00.00.01 with JobOrder file {}";
-	private static final String MSG_INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of invocation arguments: {} (only 1 allowed)";
+	private static final String MSG_INVALID_VALUE_OF_ENVVAR = "Invalid value of EnvVar: {}";
 	private static final String MSG_FILE_NOT_READABLE = "File {} is not readable";
 	private static final String MSG_ERROR_INSTANTIATING_DOCUMENT_BUILDER = "Error instantiating DocumentBuilder: {}";
 	private static final String MSG_JOF_NOT_PARSEABLE = "JobOrder file {} not parseable ({})";
+	private static final String MSG_JOF_XPATH_NOT_FOUND = "JobOrder file Xpath {} not parseable ({})";
+	
 	/** Logger for this class */
 	private static Logger logger = LoggerFactory.getLogger(SampleWrapper.class);
 	
-	/*
-	 * Check the invocation arguments: only one argument allowed, which must be a path to a JobOrder XML file
+	/** IPF XPATH-consts */
+	private static final String XPATH_LIST_OF_INPUTS = "/Ipf_Job_Order/List_of_Ipf_Procs/Ipf_Proc/List_of_Inputs/*/List_of_File_Names/File_Name";
+	//private static final String XPATH_LIST_OF_INPUTS = "/Ipf_Job_Order/List_of_Ipf_Procs/Ipf_Proc/List_of_Inputs/*/List_of_File_Names/File_Name[@FS_TYPE='S3']";
+	private static final String XPATH_LIST_OF_OUTPUTS = "/Ipf_Job_Order/List_of_Ipf_Procs/Ipf_Proc/List_of_Outputs/*/File_Name";
+	
+	/** IPF_Proc valid Tags Enums */
+    enum Ipf_Proc {Task_Name,Task_Version,List_of_Inputs,List_of_Outputs}
+		
+    /** File_Name valid Attributes Enums */
+	enum File_Name {FS_TYPE}
+		
+	/** File_Name Attribute FS_TYPE valid entries */
+	enum FS_TYPE {S3,POSIX}
+	
+	/** Environment Variables from Container (set via run-invocation or directly from docker-image)*/
+	private String ENV_FS_TYPE = System.getenv("FS_TYPE");
+	private String ENV_JOBORDER_FILE = System.getenv("JOBORDER_FILE");
+	private String ENV_S3_ENDPOINT = System.getenv("S3_ENDPOINT");
+	private String ENV_S3_ACCESS_KEY = System.getenv("S3_ACCESS_KEY");
+	private String ENV_S3_SECRET_ACCESS_KEY = System.getenv("S3_SECRET_ACCESS_KEY");
+	private String ENV_LOGFILE_TARGET = System.getenv("LOGFILE_TARGET");
+	private String ENV_STATE_CALLBACK_ENDPOINT = System.getenv("STATE_CALLBACK_ENDPOINT");
+	private String ENV_SUCCESS_STATE = System.getenv("SUCCESS_STATE");
+	private String ENV_CONTAINER_WORKDIR = System.getenv("CONTAINER_WORKDIR");
+	
+	/** Base S3-Client */
+	private static S3Client s3;
+	
+	private Boolean checkEnvFS_TYPE() {
+		for (FS_TYPE c : FS_TYPE.values()) {
+	        if (c.name().equals(ENV_FS_TYPE)) {
+	            return true;
+	        } 
+	    }
+		return false;
+	}
+	
+	private Boolean checkEnv() {
+		//check all required env-vars
+		
+		if(!checkEnvFS_TYPE()) {logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "FS_TYPE");return false;}
+		if(ENV_FS_TYPE == null || ENV_FS_TYPE.isEmpty()) {logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "FS_TYPE");return false;}
+		if(ENV_JOBORDER_FILE == null || ENV_JOBORDER_FILE.isEmpty()) {logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "JOBORDER_FILE");return false;}
+		if(ENV_FS_TYPE.equals(FS_TYPE.S3.toString()) && (ENV_S3_ENDPOINT == null || ENV_S3_ENDPOINT.isEmpty())) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "S3_ENDPOINT");return false;}
+		if(ENV_FS_TYPE.equals(FS_TYPE.S3.toString()) && (ENV_S3_ACCESS_KEY == null || ENV_S3_ACCESS_KEY.isEmpty())) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "S3_ACCESS_KEY");return false;}
+		if(ENV_FS_TYPE.equals(FS_TYPE.S3.toString()) && (ENV_S3_SECRET_ACCESS_KEY == null || ENV_S3_SECRET_ACCESS_KEY.isEmpty())) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "S3_SECRET_ACCESS_KEY");return false;}
+		if(ENV_FS_TYPE.equals(FS_TYPE.S3.toString()) && !ENV_JOBORDER_FILE.startsWith("s3://")) {logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "FS_TYPE does not allow ENV_JOBORDER_FILE");return false;};
+		if(ENV_LOGFILE_TARGET == null || ENV_LOGFILE_TARGET.isEmpty()) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "LOGFILE_TARGET");return false;}
+		if(ENV_STATE_CALLBACK_ENDPOINT == null || ENV_STATE_CALLBACK_ENDPOINT.isEmpty()) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "STATE_CALLBACK_ENDPOINT");return false;}
+		if(ENV_SUCCESS_STATE == null || ENV_SUCCESS_STATE.isEmpty()) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "SUCCESS_STATE");return false;}
+		if(ENV_CONTAINER_WORKDIR == null || ENV_CONTAINER_WORKDIR.isEmpty()) { logger.error(MSG_INVALID_VALUE_OF_ENVVAR, "CONTAINER_WORKDIR");return false;}
+		return true;
+	}
+	/**
+	 * check environment and provide JobOrderFile
 	 * 
-	 * @param args the invocation arguments
 	 * @return the JobOrder file
 	 */
-	private File checkArguments(String[] args) {
-		if (1 != args.length) {
-			logger.error(MSG_INVALID_NUMBER_OF_ARGUMENTS, args.length);
-			return null;
+	private File provideJOF() {
+		
+		String JOFContainerPath = null;
+		
+		
+		
+		// set JOF-path based on ENV
+		if (ENV_FS_TYPE.equals(FS_TYPE.S3.toString()) && ENV_JOBORDER_FILE.startsWith("s3://")) {
+			AmazonS3URI s3uri = new AmazonS3URI(ENV_JOBORDER_FILE);
+			JOFContainerPath = ENV_CONTAINER_WORKDIR+File.separator+s3uri.getKey();
+			try {
+				Files.deleteIfExists(Paths.get(JOFContainerPath));
+			} catch (IOException e) {
+				logger.error(e.getMessage());
+				return null;
+			}
 		}
-		String jobOrderFileName = args[0];
-		if (!Files.isReadable(FileSystems.getDefault().getPath(jobOrderFileName))) {
-			logger.error(MSG_FILE_NOT_READABLE, jobOrderFileName);
+		/** Set JOF-path if FS_TYPE env var (=container env var) has value `POSIX` */
+		if (ENV_FS_TYPE.equals(FS_TYPE.POSIX.toString()) && !ENV_JOBORDER_FILE.startsWith("s3://")) {
+			JOFContainerPath = ENV_JOBORDER_FILE;
+		}
+		
+		logger.info(MSG_STARTING_SAMPLE_WRAPPER, JOFContainerPath);
+		
+
+		/** Fetch JOF if container env-var `FS_TYPE` has value `S3` */
+		if (ENV_FS_TYPE.equals(FS_TYPE.S3.toString()) && ENV_JOBORDER_FILE.startsWith("s3://")) {
+			try {
+				AmazonS3URI s3uri = new AmazonS3URI(ENV_JOBORDER_FILE);
+				AwsBasicCredentials creds = AwsBasicCredentials.create( ENV_S3_ACCESS_KEY,ENV_S3_SECRET_ACCESS_KEY);
+				Region region = Region.EU_CENTRAL_1;
+				s3 = S3Client.builder()
+						.region(region)
+						.endpointOverride(URI.create(ENV_S3_ENDPOINT))
+						.credentialsProvider(StaticCredentialsProvider.create(creds))
+						.build();
+				s3.getObject(GetObjectRequest.builder()
+						.bucket(s3uri.getBucket())
+						.key(s3uri.getKey())
+						.build(),
+						ResponseTransformer.toFile(Paths.get(JOFContainerPath)));
+			} catch (software.amazon.awssdk.core.exception.SdkClientException e) {
+				logger.error(e.getMessage());
+				return null;
+			} catch (software.amazon.awssdk.services.s3.model.NoSuchKeyException e1) {
+				logger.error(e1.getMessage());
+				return null;
+			} catch (software.amazon.awssdk.services.s3.model.NoSuchBucketException e2) {
+				logger.error(e2.getMessage());
+				return null;
+			} catch (software.amazon.awssdk.services.s3.model.S3Exception e3) {
+				logger.error(e3.getMessage());
+				return null;
+			}
+		}
+		
+		/** Check if prepared JOF is readable */
+		Boolean isReadable = true;
+		try {
+			isReadable = Files.isReadable(FileSystems.getDefault().getPath(JOFContainerPath));
+		} catch (java.nio.file.InvalidPathException e) {
+			logger.error(MSG_FILE_NOT_READABLE, JOFContainerPath);
 			return null;
 		}
 		
-		return new File(jobOrderFileName);
+		if (!isReadable) {
+			logger.error(MSG_FILE_NOT_READABLE, JOFContainerPath);
+			return null;
+		}
+		return new File(JOFContainerPath);
 	}
 	
 	/**
@@ -110,6 +220,32 @@ public class SampleWrapper {
 		}
 		return jobOrderDoc;
 	}
+	/**
+	 * Get List of Input/Output Products from JOF Doc
+	 * 
+	 * @param jobOrder the Documet file to parse
+	 * @param ipfProc enum weather to list Inputs or Outputs
+	 * @return
+	 */
+	private ArrayList<String> getFileListFromJobOrderDoc(Document jobOrder, Ipf_Proc ipfProc)
+	{
+		ArrayList<String> listOfInputs=new ArrayList<String>();
+		String expression = null;;
+		try {
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			if (ipfProc == Ipf_Proc.List_of_Inputs) expression = XPATH_LIST_OF_INPUTS;
+			if (ipfProc == Ipf_Proc.List_of_Outputs) expression = XPATH_LIST_OF_OUTPUTS;
+			XPathExpression expr = xpath.compile(expression);
+			NodeList inputFiles = (NodeList) expr.evaluate(jobOrder, XPathConstants.NODESET);
+			for (int i=0; i<inputFiles.getLength();i++) {
+				listOfInputs.add(inputFiles.item(i).getTextContent()+"\n");
+			}
+		} catch (XPathExpressionException e) {
+			logger.error(MSG_JOF_XPATH_NOT_FOUND, expression, e.getMessage());
+			return null;
+		}
+		return listOfInputs;
+	}
 	
 	/*
 	 * Perform the dummy processing: check arguments, parse JobOrder file, read one input file, create one output file
@@ -117,11 +253,19 @@ public class SampleWrapper {
 	 * @param args the invocation arguments (only one argument allowed, namely the path to a JobOrder XML file)
 	 * @return the program exit code (OK or FAILURE)
 	 */
-	public int run(String[] args) {
-		logger.info(MSG_STARTING_SAMPLE_WRAPPER, (0 < args.length ? args[0] : "null"));
-		
-		// Get the JobOrder file from the invocation arguments
-		File jobOrderFile = checkArguments(args);
+	public int run() {
+
+		// ProcessorWrapperFlow
+		// ====================
+
+		// STEP [4][5] Get the JobOrder file from the invocation arguments
+		Boolean check = checkEnv();
+		if (!check) {
+			logger.info(MSG_LEAVING_SAMPLE_WRAPPER, EXIT_CODE_FAILURE, EXIT_TEXT_FAILURE);
+			return EXIT_CODE_FAILURE;
+		}
+		File jobOrderFile = provideJOF();
+
 		if (null == jobOrderFile) {
 			logger.info(MSG_LEAVING_SAMPLE_WRAPPER, EXIT_CODE_FAILURE, EXIT_TEXT_FAILURE);
 			return EXIT_CODE_FAILURE;
@@ -134,9 +278,12 @@ public class SampleWrapper {
 			return EXIT_CODE_FAILURE;
 		}
 		
-		// TODO Do something here
 		
-		// Everything went well
+		// STEP [6][7] request Inputs
+		System.out.println(getFileListFromJobOrderDoc(jobOrderDoc, Ipf_Proc.List_of_Outputs));
+		
+		
+		
 		logger.info(MSG_LEAVING_SAMPLE_WRAPPER, EXIT_CODE_OK, EXIT_TEXT_OK);
 		return EXIT_CODE_OK;
 	}
@@ -147,7 +294,7 @@ public class SampleWrapper {
 	 * @param args the invocation arguments (only one argument allowed, namely the path to a JobOrder XML file)
 	 */
 	public static void main(String[] args) {
-		System.exit((new SampleWrapper()).run(args));
+		System.exit((new SampleWrapper()).run());
 	}
 
 }
--- sample-wrapper/src/test/java/de/dlr/proseo/samplewrap/SampleWrapperTest.java
+++ sample-wrapper/src/test/java/de/dlr/proseo/samplewrap/SampleWrapperTest.java
@@ -8,8 +8,12 @@ package de.dlr.proseo.samplewrap;
 import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -25,7 +29,39 @@ import org.junit.Test;
  */
 public class SampleWrapperTest {
 	
-	private static final String JOB_ORDER_FILE_NAME = "src/test/resources/sample-joborder.xml";
+	private static final String JOB_ORDER_FILE_NAME = "src/test/resources/JobOrder.609521551_KNMI.xml";
+	/**
+	 * Used for setting required ENV VARS before TEST-RUN
+	 * @param newenv Map<String, String> env-var key&value
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	protected static void setEnv(Map<String, String> newenv) throws Exception {
+		try {
+			Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+			Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+			theEnvironmentField.setAccessible(true);
+			Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+			env.putAll(newenv);
+			Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+			theCaseInsensitiveEnvironmentField.setAccessible(true);
+			Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
+			cienv.putAll(newenv);
+		} catch (NoSuchFieldException e) {
+			Class[] classes = Collections.class.getDeclaredClasses();
+			Map<String, String> env = System.getenv();
+			for(Class cl : classes) {
+				if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+					Field field = cl.getDeclaredField("m");
+					field.setAccessible(true);
+					Object obj = field.get(env);
+					Map<String, String> map = (Map<String, String>) obj;
+					map.clear();
+					map.putAll(newenv);
+				}
+			}
+		}
+	}
+	
 
 	/**
 	 * Make sure all input files exist, and all output files are removed
@@ -38,7 +74,6 @@ public class SampleWrapperTest {
 			throw new FileNotFoundException(JOB_ORDER_FILE_NAME);
 		}
 	}
-
 	/**
 	 * Currently not in use
 	 * 
@@ -54,8 +89,25 @@ public class SampleWrapperTest {
 	 * @throws java.lang.Exception never
 	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp1() throws Exception {
+	
+		Map<String,String> map1 = new HashMap<>();
+		map1.put("FS_TYPE", "POSIX");
+		map1.put("JOBORDER_FILE", JOB_ORDER_FILE_NAME);
+		map1.put("S3_ENDPOINT", "test");
+		map1.put("S3_ACCESS_KEY", "test");
+		map1.put("S3_SECRET_ACCESS_KEY", "test");
+		map1.put("LOGFILE_TARGET", "test");
+		map1.put("STATE_CALLBACK_ENDPOINT", "test");
+		map1.put("SUCCESS_STATE", "test");
+		map1.put("CONTAINER_WORKDIR", "test");
+		try {
+			setEnv(map1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+	
 
 	/**
 	 * Clean up: Remove generated output products
@@ -70,14 +122,85 @@ public class SampleWrapperTest {
 	 * Test method for {@link de.dlr.proseo.sampleproc.SampleProcessor#run(java.lang.String[])}.
 	 */
 	@Test
-	public final void testRun() {
-		String[] args = { JOB_ORDER_FILE_NAME };
-		int rc = (new SampleWrapper()).run(args); 
+	public final void testRun1() {
+		
+		// Hashmap holding env-vars
+		Map<String,String> envmap = new HashMap<>();
+		
+		
+		/** working run using POSIX-JOF */
+		int rc = (new SampleWrapper()).run(); 
 		assertEquals("Return code should be 0", 0L, (long) rc);
 		
-		String[] args1 = { "invalid_file_name" };
-		rc = (new SampleWrapper()).run(args1); 
+		/** JOF-path is wrong */
+		
+		envmap.put("JOBORDER_FILE", "adadad");
+		try {
+			setEnv(envmap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		rc = (new SampleWrapper()).run(); 
 		assertEquals("Return code should be 255", 255L, (long) rc);
+		
+		/** FS_TYPE is wrong */
+		envmap.clear();
+		envmap.put("JOBORDER_FILE", JOB_ORDER_FILE_NAME);
+		envmap.put("FS_TYPE", "sfsf");
+		try {
+			setEnv(envmap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		rc = (new SampleWrapper()).run(); 
+		assertEquals("Return code should be 255", 255L, (long) rc);
+		
+		/** FS_TYPE is S3, but JOBORDER_FILE refers to non s3 URI */
+		envmap.clear();
+		envmap.put("JOBORDER_FILE", JOB_ORDER_FILE_NAME);
+		envmap.put("FS_TYPE", "S3");
+		try {
+			setEnv(envmap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		rc = (new SampleWrapper()).run(); 
+		assertEquals("Return code should be 255", 255L, (long) rc);
+		
+		/** working run using S3-JOF */
+		envmap.clear();
+		envmap.put("JOBORDER_FILE", "s3://joborders/JobOrder.609521551_KNMI.xml");
+		envmap.put("FS_TYPE", "S3");
+		envmap.put("S3_ENDPOINT", "http://localhost:9000");
+		envmap.put("S3_ACCESS_KEY", "short_access_key");
+		envmap.put("S3_SECRET_ACCESS_KEY", "short_secret_key");
+		envmap.put("CONTAINER_WORKDIR", "target");
+		try {
+			setEnv(envmap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		rc = (new SampleWrapper()).run(); 
+		assertEquals("Return code should be 0", 0L, (long) rc);
+		
+		/** S3-JOF but using wrong s3-credentials*/
+		envmap.clear();
+		envmap.put("JOBORDER_FILE", "s3://joborders/JobOrder.609521551_KNMI.xml");
+		envmap.put("FS_TYPE", "S3");
+		envmap.put("S3_ENDPOINT", "http://localhost:9000");
+		envmap.put("S3_ACCESS_KEY", "adad");
+		envmap.put("S3_SECRET_ACCESS_KEY", "weewe");
+		envmap.put("CONTAINER_WORKDIR", "target");
+		try {
+			setEnv(envmap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		rc = (new SampleWrapper()).run(); 
+		assertEquals("Return code should be 255", 255L, (long) rc);
+		
+		
 	}
+	
 
 }
