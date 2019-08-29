6407e3e26e082c4ece08e5361274f6b66d8c7fdd
Thomas Bassler <thomas.bassler@drbassler.de>
2019-08-29 15:59:12 +0200

    … getters and setters added in ProcessingFacility

--- model/src/main/java/de/dlr/proseo/model/ProcessingFacility.java
+++ model/src/main/java/de/dlr/proseo/model/ProcessingFacility.java
@@ -6,10 +6,7 @@
 package de.dlr.proseo.model;
 
 import java.util.Objects;
-import java.util.Set;
-
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 
 /**
  * A processing facility for running prosEO jobs and storing prosEO product files,  e. g. at a cloud service provider.
@@ -59,6 +56,24 @@ public class ProcessingFacility extends PersistentObject {
 	 */
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	/**
+	 * Gets the URL of the processing facility (Kubernetes instance)
+	 * 
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * Sets the URL of the processing facility (Kubernetes instance)
+	 * 
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 	@Override
