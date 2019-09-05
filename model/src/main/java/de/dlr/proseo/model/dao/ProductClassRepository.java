ffb574b1d675d895b5350b9db433d88a535138f5
Thomas Bassler <thomas.bassler@drbassler.de>
2019-09-05 11:41:12 +0200

    Method comments updated in ProductClassRepository

--- model/src/main/java/de/dlr/proseo/model/dao/ProductClassRepository.java
+++ model/src/main/java/de/dlr/proseo/model/dao/ProductClassRepository.java
@@ -22,28 +22,45 @@ public interface ProductClassRepository extends CrudRepository<ProductClass, Lon
 	
 	/**
 	 * Find all product classes for a given mission code
+	 * 
+	 * @param missionCode the code of the mission
+	 * @return a list of product classes
 	 */
 	@Query("select pc from ProductClass pc where pc.mission.code = ?1")
 	public List<ProductClass> findByMissionCode(String missionCode);
 	
 	/**
 	 * Find all product classes for a given product type (at most one per mission)
+	 * 
+	 * @param productType the (prosEO-internal) product type
+	 * @return a list of product classes
 	 */
 	public List<ProductClass> findByProductType(String productType);
 	
 	/**
 	 * Find all product classes for a given mission-defined type (at most one per mission)
+	 * 
+	 * @param missionType
+	 * @return a list of product classes
 	 */
 	public List<ProductClass> findByMissionType(String missionType);
 	
 	/**
 	 * Find a product class by mission code and product type
+	 * 
+	 * @param missionCode the code of the mission
+	 * @param productType the (prosEO-internal) product type
+	 * @return the unique product class identified by the mission code and product type
 	 */
 	@Query("select pc from ProductClass pc where pc.mission.code = ?1 and pc.productType = ?2")
 	public ProductClass findByMissionCodeAndProductType(String missionCode, String productType);
 	
 	/**
 	 * Find a product class by mission code and mission-defined type
+	 * 
+	 * @param missionCode the code of the mission
+	 * @param missionType the mission-defined type of the product class
+	 * @return the unique product class identified by the mission code and mission-defined type
 	 */
 	@Query("select pc from ProductClass pc where pc.mission.code = ?1 and pc.missionType = ?2")
 	public ProductClass findByMissionCodeAndMissionType(String missionCode, String missionType);
