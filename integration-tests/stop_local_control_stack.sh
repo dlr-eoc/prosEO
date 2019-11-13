aed6347b4ae39778bfbdae65e4e0f78ced34c8cf
Hubert Asamer <hubert.asamer@dlr.de>
2019-11-13 14:51:07 +0100

    integration-tests: added stack stop script

---
+++ integration-tests/stop_local_control_stack.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [[ "$#" -eq 0 || "$#" -ne 2 ]]
+  then
+    echo "Invalid number of args..."
+    echo "Usage: $0 <registry-url> <proseo-revison>"
+    echo "* registry-url: a valid docker-registry URL where the referenced docker-images are registered"
+    echo "* proseo-revison: a valid git-revision"
+    echo ""
+    echo "-->used compose file is:"
+    cat docker-compose.yml
+    exit 1
+fi
+
+# check if registry-login is successful
+
+if docker login $1; then
+  echo "registry login successful..."
+else
+  echo "could not login to given registry-url"
+  exit 1
+fi
+
+export REGISTRY_URL=$1
+export PROSEO_REVISION=$2
+
+docker-compose down
