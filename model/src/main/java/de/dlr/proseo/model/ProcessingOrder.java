c286c5f924f460acfa024891569d47e73a2d4124
melchinger <melchinger@prophos.de>
2019-11-26 11:27:55 +0100

    New parameters stdoutLogLevel and stderrLogLevel in JobStep. "APPROVED" added in enum OrderState.

--- model/src/main/java/de/dlr/proseo/model/ProcessingOrder.java
+++ model/src/main/java/de/dlr/proseo/model/ProcessingOrder.java
@@ -129,17 +129,19 @@ public class ProcessingOrder extends PersistentObject {
 	/**
 	 * Possible states for a processing order; recommended state transitions:
 	 * <ol>
-	 * <li>INITIAL -&gt; RELEASED: Customer approved order parameters and/or committed budget</li>
-	 * <li>RELEASED -&gt; PLANNED: Jobs for the processing order have been generated</li>
-	 * <li>PLANNED -&gt; RUNNING: The first jobs have started, further jobs can be started</li>
-	 * <li>RUNNING -&gt; PLANNED: Order execution halted, no further jobs will be started (started jobs will be completed,
-	 *     if they are not halted themselves)</li>
+	 *
+	 * <li>INITIAL -&gt; APPROVED: Customer approved order parameters and/or committed budget
+	 * <li>APPROVED -&gt; PLANNED: Jobs for the processing order have been generated</li>
+	 * <li>PLANNED -&gt; RELEASED: The order is released for running as planned</li>
+	 * <li>RELEASED -&gt; RUNNING: The first jobs have started, further jobs can be started</li>
+	 * <li>RUNNING -&gt; PLANNED: Order execution halted, no further jobs will be started (started jobs will be completed, if they are not halted themselves)</li>
 	 * <li>RUNNING -&gt; COMPLETED: All jobs have been completed successfully</li>
 	 * <li>RUNNING -&gt; FAILED: All jobs have been completed, but at least one of them failed</li>
-	 * <li>COMPLETED/FAILED -&gt; CLOSED: Delivery has been acknowledged by customer and/or order fee has been paid</li>
+	 * <li>PLANNED -&gt; FAILED: The order was cancelled by the operator</li>
+	 * <li>COMPLETED/FAILED -&gt;; CLOSED: Delivery/failure has been acknowledged by customer and/or order fee has been paid</li>
 	 * </ol>
 	 */
-	public enum OrderState { INITIAL, RELEASED, PLANNED, RUNNING, COMPLETED, FAILED, CLOSED };
+	public enum OrderState { INITIAL, APPROVED, PLANNED, RELEASED, RUNNING, COMPLETED, FAILED, CLOSED };
 	
 	/**
 	 * Possible methods for partitioning the order time period into individual job time periods for product generation:
