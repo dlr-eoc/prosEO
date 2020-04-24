28d9d287d37200ad509f19204970375355edd63a
Thomas Bassler <thomas.bassler@drbassler.de>
2020-04-24 14:36:54 +0200

    Single-node deployment for processing engine created

---
+++ processing-engine/README.md
@@ -0,0 +1,241 @@
+prosEO Processing Facility Deployment
+=====================================
+
+This document describes how to deploy a prosEO processing facility on a new set of
+(virtual) machines, e. g. as provisioned by some cloud service provider.
+
+# Installation Requirements
+The (virtual) machines need to at least fulfil the following requirements:
+- Linux installed
+- Ability to run Docker and Kubernetes (i. e. standard CentOS kernel, no provider-specific builds)
+- TBD GB RAM
+- TBD GB online storage
+- more TBD
+- Optional: Access to some object storage conforming to the AWS S3 protocol
+
+# Step 1: Install Docker and Kubernetes
+TBD
+
+# Step 2: Deploy the prosEO Storage Manager
+TBD
+
+# Step 3 (optional): Deploy Alluxio as Storage Provider
+TBD
+
+# Step 4 (optional): Deploy Minio as S3 Object Storage Provider
+TBD
+
+# Configure prosEO
+Add the new storage facility to the prosEO Control Instance configuration:
+
+## Alternative 1: Using Command Line Interface
+Run a prosEO CLI connected to the prosEO Control Instance to configure. Log in with
+facility management privileges and issue the following command (note the quotes around the
+description parameter to include blanks in the description):
+```
+facility create <facility name> 'description=<any description>' processingEngineUrl=https://<K8S host>:<K8S port>/ storageManagerUrl=http://<K8S host>:<K8S port>/api/v1/namespaces/default/services/storage-mgr-service:service/proxy/proseo/storage-mgr/v1 defaultStorageType=[POSIX|S3|ALLUXIO]
+```
+
+Alternatively create a JSON file describing the processing facility:
+```json
+{
+  "name" : "<facility name>",
+  "description" : "<any description>",
+  "processingEngineUrl" : "https://<K8S host>:<K8S port>/",
+  "storageManagerUrl" : "http://<K8S host>:<K8S port>/api/v1/namespaces/default/services/storage-mgr-service:service/proxy/proseo/storage-mgr/v1",
+  "defaultStorageType" : "[POSIX|S3|ALLUXIO]"
+}
+```
+
+Then in the prosEO CLI issue the command:
+```
+facility create --file=<JSON file name>
+```
+
+## Alternative 2: Using the prosEO GUI
+Not yet implemented.
+
+# Example: Setting up a Single-Node Processing Facility
+Assuming there is a MacOS or Windows machine available with sufficient RAM and disk capacity, a single-node setup
+can be achieved for both the prosEO Control Instance and the prosEO Processing Facility. The following
+example shows a setup based on MacOS.
+
+Note that for the following to work the target machine must have unrestricted access
+to resources on the Internet.
+
+## Step 1: Install Docker and Kubernetes
+Download Docker Desktop from <https://www.docker.com/products/docker-desktop>.
+
+Install and run Docker Desktop and activate Kubernetes as described here:
+- For MacOS:
+  1) Install and run: <https://docs.docker.com/docker-for-mac/install/>
+  2) Activate Kubernetes: <https://docs.docker.com/docker-for-mac/>
+- For Windows:
+  1) Install and run: <https://docs.docker.com/docker-for-windows/install/>
+  2) Activate Kubernetes: <https://docs.docker.com/docker-for-windows/>
+
+Deploy and run a Kubernetes dashboard without requiring a login:
+1) Download the recommended dashboard configuration from <https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml>
+2) Copy the recommended configuration to a new file `kubernetes-dashboard.yaml`.
+3) Edit `kubernetes-dashboard.yaml`:
+   a) Locate the deployment entry for `kubernetes-dashboard` (around line 170)
+   b) Locate the subsection `spec.template.spec.containers.args` and add entries for
+      `--enable-skip-login` and `--disable-settings-authorizer`
+4) Run the dashboard:
+   ```
+   kubectl apply -f kubernetes-dashboard.yaml
+   kubectl proxy --accept-hosts='.*' &
+   ```
+5) Access the dashboard at <http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/>.
+
+## Step 2: Deploy the prosEO Storage Manager
+For the single-node installation we will use POSIX as the default file system, thereby
+avoiding the overhead of running an S3 object storage provider. We will not make Alluxio
+available either. This does not place any functional constraints on prosEO, since these
+storage options are meant for externally provided installations only, where online storage
+is expensive (a multi-TB USB-3 disk on a laptop is not).
+
+1) Locate the file `storage-mgr.yaml` and edit the image reference (around line 20) to point
+   to your preferred prosEO repository.
+2) Run the Storage Manager:
+   ```
+   kubectl apply -f <path to file>/storage-mgr.yaml
+   ```
+
+## Step 3: Deploy the prosEO Control Instance
+Create the prosEO Control Instance from a `docker-compose.yml` file like the following:
+```yaml
+version: '3'
+services:
+  proseo-db:
+    image: postgres:11
+    environment:
+      - POSTGRES_DB=proseo
+    volumes:
+      - pgdata:/var/lib/postgresql/data
+    ports:
+      - "5432:5432"
+  proseo-ingestor:
+    image: ${REGISTRY_URL}/proseo-ingestor:${PROSEO_VERSION}
+    ports:
+      - "8081:8080"
+    depends_on:
+      - proseo-db
+  proseo-order-mgr:
+    image: ${REGISTRY_URL}/proseo-order-mgr:${PROSEO_VERSION}
+    ports:
+      - "8082:8080"
+    depends_on:
+      - proseo-db
+  proseo-prodplanner:
+    image: ${REGISTRY_URL}/proseo-planner:${PROSEO_VERSION}
+    ports:
+      - "8083:8080"
+    depends_on:
+      - proseo-db
+  proseo-processor-mgr:
+    image: ${REGISTRY_URL}/proseo-processor-mgr:${PROSEO_VERSION}
+    ports:
+      - "8084:8080"
+    depends_on:
+      - proseo-db
+  proseo-productclass-mgr:
+    image: ${REGISTRY_URL}/proseo-productclass-mgr:${PROSEO_VERSION}
+    ports:
+      - "8085:8080"
+    depends_on:
+      - proseo-db
+  proseo-user-mgr:
+    image: ${REGISTRY_URL}/proseo-user-mgr:${PROSEO_VERSION}
+    ports:
+      - "8086:8080"
+    depends_on:
+      - proseo-db
+  proseo-facility-mgr:
+    image: ${REGISTRY_URL}/proseo-facility-mgr:${PROSEO_VERSION}
+    ports:
+      - "8087:8080"
+    depends_on:
+      - proseo-db
+  proseo-gui:
+    image: ${REGISTRY_URL}/proseo-ui-gui:${PROSEO_VERSION}
+    ports:
+      - "8088:8080"
+    depends_on:
+      - proseo-ingestor
+      - proseo-prodplanner
+      - proseo-order-mgr
+      - proseo-processor-mgr
+      - proseo-productclass-mgr
+  proseo-pgadmin:
+    image: dpage/pgadmin4
+    environment:
+      - PGADMIN_ENABLE_TLS=True
+      - PGADMIN_DEFAULT_EMAIL=${PGADMIN_EMAIL}
+      - PGADMIN_DEFAULT_PASSWORD=${PGADMIN_PASSWORD}
+    volumes:
+      - "./proseo-components/proseo-pgadmin/certs/proseo-selfsigned.crt:/certs/server.cert"
+      - "./proseo-components/proseo-pgadmin/certs/proseo-selfsigned.key:/certs/server.key"
+    ports:
+      - "8443:443"
+    depends_on:
+      - proseo-db
+volumes:
+  pgdata:
+networks:
+  default:
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 172.177.57.0/24
+```
+
+To run the Control Instance issue the following commands from the directory, where the
+`docker-compose.yml` file resides:
+```
+export REGISTRY_URL=<your preferred prosEO repository>
+export PROSEO_VERSION=<the prosEO version to install>
+export POSTGRES_PASSWORD=<password for postgres user as configured in prosEO images>
+export PGADMIN_EMAIL=<email address for pgAdmin authentication>
+export PGADMIN_PASSWORD=<some password for pgAdmin authentication>
+docker-compose up -d
+```
+
+# Step 4: Install and Run the prosEO Command Line Interface
+The current version of the prosEO Command Line Interface (CLI) can be downloaded from
+<https://proseo-registry.eoc.dlr.de/artifactory/proseo/proseo-ui-cli.jar>. The CLI must
+be configured with a YAML file to be able to connect to the prosEO Control Instance.
+A sample configuration file can be downloaded from <https://proseo-registry.eoc.dlr.de/artifactory/proseo/application.yml.sample>.
+The settings in this file are suitable for the Docker Compose configuration shown above.
+
+Start the CLI with `java -jar proseo-ui-cli.jar`. On a newly created prosEO Control Instance
+a default user `sysadm` (password `sysadm`) is provided.
+
+To initialize the prosEO instance, run the following CLI commands:
+```
+login -usysadm -psysadm
+user update sysadm password=<new password>
+create mission PTM 'name=prosEO Test Mission'
+```
+
+
+# Step 5: Configure the prosEO Processing Facility
+Create a JSON file `facility.json` describing the local processing facility:
+```
+{
+  "name" : "localhost",
+  "description" : "Docker Desktop Minikube",
+  "processingEngineUrl" : "http://host.docker.internal:8001/",
+  "storageManagerUrl" : "http://host.docker.internal:8001/api/v1/namespaces/default/services/storage-mgr-service:service/proxy/proseo/storage-mgr/v1",
+  "defaultStorageType" : "POSIX"
+}
+```
+
+On the prosEO CLI issue the following commands:
+```
+login -usysadm -p<your password> PTM
+facility create --file=facility.json
+```
+
+You are now ready to configure your first mission. See <https://github.com/dlr-eoc/prosEO/tree/master/samples/testdata> for
+an example, which works with the prosEO Sample Processor.
---
+++ processing-engine/single-node-deploy/create_data_local.sh
@@ -0,0 +1,531 @@
+#!/bin/bash
+#
+# create_data.sh
+# --------------
+#
+# Create dynamic test data for the prosEO test mission:
+# - L0 input data
+# - IERSB AUX input data
+# - a processing facility on a Telekom OTC cluster
+# - a processing order for L2 products
+# - a processing order for L3 products
+#
+
+# Create a new CLI command script
+CLI_SCRIPT=cli_data_script.txt
+echo "" >$CLI_SCRIPT
+ 
+# Create empty subdirectory for test data
+TEST_DATA_DIR=testfiles
+mkdir -p $TEST_DATA_DIR
+cd $TEST_DATA_DIR
+rm *
+
+# Create L0 input data
+# Products consist of the fields id, type, start time, stop time, generation time and revision,
+# separated by vertical bars
+# L0 products are in 45 min slices, starting with orbit 3000
+cat >PTM_L0_20191104090000_20191104094500_20191104120000.RAW <<EOF
+1234567|L0________|2019-11-04T09:00:00Z|2019-11-04T09:45:00Z|2019-11-04T12:00:00Z|1
+EOF
+
+cat >PTM_L0_20191104094500_20191104103000_20191104120100.RAW <<EOF
+1234567|L0________|2019-11-04T09:45:00Z|2019-11-04T10:30:00Z|2019-11-04T12:01:00Z|1
+EOF
+
+cat >PTM_L0_20191104103000_20191104111500_20191104120200.RAW <<EOF
+1234567|L0________|2019-11-04T10:30:00Z|2019-11-04T11:15:00Z|2019-11-04T12:02:00Z|1
+EOF
+
+cat >PTM_L0_20191104111500_20191104120000_20191104120300.RAW <<EOF
+1234567|L0________|2019-11-04T11:15:00Z|2019-11-04T12:00:00Z|2019-11-04T12:03:00Z|1
+EOF
+
+cat >PTM_L0_20191104120000_20191104124500_20191104150000.RAW <<EOF
+1234567|L0________|2019-11-04T12:00:00Z|2019-11-04T12:45:00Z|2019-11-04T15:00:00Z|1
+EOF
+
+cat >PTM_L0_20191104124500_20191104133000_20191104150100.RAW <<EOF
+1234567|L0________|2019-11-04T12:45:00Z|2019-11-04T13:30:00Z|2019-11-04T15:01:00Z|1
+EOF
+
+cat >PTM_L0_20191104133000_20191104141500_20191104150200.RAW <<EOF
+1234567|L0________|2019-11-04T13:30:00Z|2019-11-04T14:15:00Z|2019-11-04T15:02:00Z|1
+EOF
+
+cat >PTM_L0_20191104141500_20191104150000_20191104150300.RAW <<EOF
+1234567|L0________|2019-11-04T14:15:00Z|2019-11-04T15:00:00Z|2019-11-04T15:03:00Z|1
+EOF
+
+cat >PTM_L0_20191104150000_20191104154500_20191104180000.RAW <<EOF
+1234567|L0________|2019-11-04T15:00:00Z|2019-11-04T15:45:00Z|2019-11-04T18:00:00Z|1
+EOF
+
+cat >PTM_L0_20191104154500_20191104163000_20191104180100.RAW <<EOF
+1234567|L0________|2019-11-04T15:45:00Z|2019-11-04T16:30:00Z|2019-11-04T18:01:00Z|1
+EOF
+
+cat >PTM_L0_20191104163000_20191104171500_20191104180200.RAW <<EOF
+1234567|L0________|2019-11-04T16:30:00Z|2019-11-04T17:15:00Z|2019-11-04T18:02:00Z|1
+EOF
+
+cat >PTM_L0_20191104171500_20191104180000_20191104180300.RAW <<EOF
+1234567|L0________|2019-11-04T17:15:00Z|2019-11-04T18:00:00Z|2019-11-04T18:03:00Z|1
+EOF
+
+cat >PTM_L0_20191104180000_20191104184500_20191104210000.RAW <<EOF
+1234567|L0________|2019-11-04T18:00:00Z|2019-11-04T18:45:00Z|2019-11-04T21:00:00Z|1
+EOF
+
+cat >PTM_L0_20191104184500_20191104193000_20191104210100.RAW <<EOF
+1234567|L0________|2019-11-04T18:45:00Z|2019-11-04T19:30:00Z|2019-11-04T21:01:00Z|1
+EOF
+
+cd -
+
+# IERSB AUX products are real-world data:
+# Using bulletinb-380.xml
+cp -p bulletinb-380.xml $TEST_DATA_DIR
+
+# Create the storage manager in the local Minikube
+kubectl apply -f storage-mgr-local.yaml
+
+# Create a dashboard at http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+kubectl apply -f kubernetes-dashboard.yaml
+
+# Upload test data to POSIX storage on Minikube
+kubectl exec storage-mgr-f6kh9 mkdir -p /opt/s5p/data/integration-test
+kubectl cp $TEST_DATA_DIR storage-mgr-f6kh9:/opt/s5p/data/integration-test/testdata
+
+# Create a processing facility
+echo "facility create localhost 'description=Docker Desktop Minikube' processingEngineUrl=https://localhost:8001/ storageManagerUrl=http://localhost:8001/api/v1/namespaces/default/services/storage-mgr-service:service/proxy/proseo/storage-mgr/v1 defaultStorageType=POSIX" >>$CLI_SCRIPT
+
+# Ingest test data into prosEO
+echo "ingest --file=$TEST_DATA_DIR/ingest_products.json localhost" >>$CLI_SCRIPT
+cat >$TEST_DATA_DIR/ingest_products.json <<EOF
+[
+    {
+        "missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T09:00:00.000000",
+        "sensingStopTime": "2019-11-04T09:45:00.000000",
+        "generationTime": "2019-11-04T12:00:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104090000_20191104094500_20191104120000.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T09:45:00.000000",
+        "sensingStopTime": "2019-11-04T10:30:00.000000",
+        "generationTime": "2019-11-04T12:01:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104094500_20191104103000_20191104120100.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T10:30:00.000000",
+        "sensingStopTime": "2019-11-04T11:15:00.000000",
+        "generationTime": "2019-11-04T12:02:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104103000_20191104111500_20191104120200.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T11:15:00.000000",
+        "sensingStopTime": "2019-11-04T12:00:00.000000",
+        "generationTime": "2019-11-04T12:03:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104111500_20191104120000_20191104120300.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T12:00:00.000000",
+        "sensingStopTime": "2019-11-04T12:45:00.000000",
+        "generationTime": "2019-11-04T15:00:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104120000_20191104124500_20191104150000.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T12:45:00.000000",
+        "sensingStopTime": "2019-11-04T13:30:00.000000",
+        "generationTime": "2019-11-04T15:01:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104124500_20191104133000_20191104150100.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T13:30:00.000000",
+        "sensingStopTime": "2019-11-04T14:15:00.000000",
+        "generationTime": "2019-11-04T15:02:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104133000_20191104141500_20191104150200.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T14:15:00.000000",
+        "sensingStopTime": "2019-11-04T15:00:00.000000",
+        "generationTime": "2019-11-04T15:03:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104141500_20191104150000_20191104150300.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T15:00:00.000000",
+        "sensingStopTime": "2019-11-04T15:45:00.000000",
+        "generationTime": "2019-11-04T18:00:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104150000_20191104154500_20191104180000.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T15:45:00.000000",
+        "sensingStopTime": "2019-11-04T16:30:00.000000",
+        "generationTime": "2019-11-04T18:01:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104154500_20191104163000_20191104180100.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T16:30:00.000000",
+        "sensingStopTime": "2019-11-04T17:15:00.000000",
+        "generationTime": "2019-11-04T18:02:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104163000_20191104171500_20191104180200.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T17:15:00.000000",
+        "sensingStopTime": "2019-11-04T18:00:00.000000",
+        "generationTime": "2019-11-04T18:03:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104171500_20191104180000_20191104180300.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T18:00:00.000000",
+        "sensingStopTime": "2019-11-04T18:45:00.000000",
+        "generationTime": "2019-11-04T21:00:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104180000_20191104184500_20191104210000.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "L0________",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-11-04T18:45:00.000000",
+        "sensingStopTime": "2019-11-04T19:30:00.000000",
+        "generationTime": "2019-11-04T21:01:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "PTM_L0_20191104184500_20191104193000_20191104210100.RAW",
+        "auxFileNames": []
+    },
+    {
+		"missionCode": "PTM",
+        "productClass": "AUX_IERS_B",
+        "fileClass": "OPER",
+        "mode": "OPER",
+        "productQuality": "TEST",
+        "sensingStartTime": "2019-09-01T00:00:00.000000",
+        "sensingStopTime": "2019-10-01T00:00:00.000000",
+        "generationTime": "2019-10-01T12:00:00.000000",
+        "parameters": [
+            {
+                "key": "revision",
+                "parameterType": "INTEGER",
+                "parameterValue": "1"
+            }
+        ],
+        "sourceStorageType": "POSIX",
+        "mountPoint": "/opt/s5p/data",
+        "filePath": "integration-test/testdata",
+        "productFileName": "bulletinb-380.xml",
+        "auxFileNames": []
+    }
+]
+EOF
+
+# Create a L2 processing order
+echo "order create --file=$TEST_DATA_DIR/order_l2.json" >>$CLI_SCRIPT
+cat >$TEST_DATA_DIR/order_l2.json <<EOF
+{
+    "missionCode": "PTM",
+    "identifier": "L2_orbits_3000-3002",
+    "orderState": "INITIAL",
+    "slicingType": "ORBIT",
+    "sliceOverlap": 0,
+    "filterConditions": [
+        {
+            "key": "revision",
+            "parameterType": "INTEGER",
+            "parameterValue": "1"
+        },
+        {
+            "key": "fileClass",
+            "parameterType": "STRING",
+            "parameterValue": "OPER"
+        }
+    ],
+    "outputParameters": [
+        {
+            "key": "revision",
+            "parameterType": "INTEGER",
+            "parameterValue": "99"
+        },
+        {
+            "key": "copernicusCollection",
+            "parameterType": "INTEGER",
+            "parameterValue": "77"
+        }
+    ],
+    "configuredProcessors": [ "PTML2_0.1.0_OPER_2020-03-25" ],
+    "orbits": [
+        { "spacecraftCode": "PTS", "orbitNumberFrom": 3000, "orbitNumberTo": 3002 }
+    ],
+    "requestedProductClasses": [ "PTM_L2A", "PTM_L2B" ],
+    "inputProductClasses": [],
+    "outputFileClass": "TEST",
+    "processingMode": "OPER"
+}
+EOF
+
+# Create a L3 processing order
+echo "order create --file=$TEST_DATA_DIR/order_l3.json" >>$CLI_SCRIPT
+cat >$TEST_DATA_DIR/order_l3.json <<EOF
+{
+    "missionCode": "PTM",
+    "identifier": "L3_products_9:30-17:30",
+    "orderState": "INITIAL",
+    "startTime": "2019-11-04T09:30:00",
+    "stopTime": "2019-11-04T17:00:00",
+    "slicingType": "TIME_SLICE",
+    "sliceDuration": 14400,
+    "sliceOverlap": 0,
+    "propagateSlicing": false,
+    "filterConditions": [
+        {
+            "key": "revision",
+            "parameterType": "INTEGER",
+            "parameterValue": "99"
+        },
+        {
+            "key": "fileClass",
+            "parameterType": "STRING",
+            "parameterValue": "TEST"
+        }
+    ],
+    "outputParameters": [
+        {
+            "key": "revision",
+            "parameterType": "INTEGER",
+            "parameterValue": "99"
+        },
+        {
+            "key": "copernicusCollection",
+            "parameterType": "INTEGER",
+            "parameterValue": "77"
+        }
+    ],
+    "configuredProcessors": [ "PTML3_0.1.0_OPER_2020-03-25" ],
+    "orbits": [],
+    "requestedProductClasses": [ "PTM_L3" ],
+    "inputProductClasses": [ "PTM_L2A", "PTM_L2B" ],
+    "outputFileClass": "TEST",
+    "processingMode": "OPER"
+}
+EOF
+
+echo "Test data generation complete."
+echo "Execute 'java -jar <path to CLI> < $CLI_SCRIPT' to load the data into prosEO."
