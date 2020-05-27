26caf6c4323d4dadff6793dab53e29650c04f9bb
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-05-27 18:11:26 +0200

    For convenience, generate the run.sh script from build.sh

--- processing-engine/k8s-deploy/build.sh
+++ processing-engine/k8s-deploy/build.sh
@@ -10,6 +10,11 @@ NC='\033[0m' # No Color
 if [ -f terraform/.ostackrc -a -f ssh-keys/cluster-key.pem -a -f ssh-keys/cluster-key.pub ];then
   docker build -t proseo-k8s-deployer:$BUILD .
   echo -e ${YELLOW}"**build finished -> proseo-k8s-deployer:$BUILD"${NC}
+  cat >run.sh <<EOF
+#!/bin/bash
+docker run --rm -it -v $(pwd)/terraform/state:/terraform/state proseo-k8s-deployer:$BUILD
+EOF
+  chmod +x run.sh
 else
   echo "one or more of terraform/.ostackrc, ssh-keys/cluster-key.pem or ssh-keys/cluster-key.pub is missing..."
 fi
