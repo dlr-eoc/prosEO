872908cc26f2d6a4b18d2d522e92f0c3acec72f5
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-07-02 10:07:09 +0200

    Make scripts available to pipenv and docker

--- processing-engine/k8s-deploy/docker/run.sh
+++ processing-engine/k8s-deploy/docker/run.sh
@@ -4,5 +4,5 @@ export WORK_DIR=/prosEO
 
 docker run --rm -it \
   -v "$PWD/kubespray:$WORK_DIR/kubespray" \
-  -v "$PWD/clusters:$WORK_DIR/clusters" \
+  -v "$PWD/scripts:$WORK_DIR/scripts" \
     proseo-k8s-deployer:latest
---
+++ processing-engine/k8s-deploy/scripts/make_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+CLUSTER="$1"
+if [ -z "$CLUSTER" ] ; then
+  echo "Usage: make_cluster.sh <clustername>"
+fi
+
+# Create cluster directory from kubespray sample
+CLUSTERDIR="kubespray/inventory/$CLUSTER"
+cp -rf kubespray/inventory/sample "$CLUSTERDIR"
+cd "$CLUSTERDIR"
+ln -sf ../../contrib
+cd -
+
+# Create autoenv configuration
+CONFIGDIR="$CLUSTERDIR/config"
+mkdir -p "$CONFIGDIR"
+cp templates/autoenv "$CLUSTERDIR/.autoenv"
+
+# Configure cluster name for bash prompt
+cat >"$CONFIGDIR/clustername.sh" <<EOF
+export PROSEO_CLUSTER_NAME="$CLUSTER"
+EOF
+
+echo "Bare cluster configured in: $CLUSTERDIR"
