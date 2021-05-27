ea00e4f08f9e499cf05fdb564ac10b6cf17078db
Thomas Bassler <thomas.bassler@drbassler.de>
2021-05-27 10:33:55 +0200

    Docker image version changed to 0.7.3 in sample test data

--- samples/testdata/configure_proseo_test_mission.pl
+++ samples/testdata/configure_proseo_test_mission.pl
@@ -163,7 +163,7 @@ my @processors = (
     	tasks => [ 
     	   { taskName => 'ptm_l01b', taskVersion => '0.1.0' }
     	],
-    	dockerImage => 'localhost:5000/proseo-sample-wrapper:0.7.1'
+    	dockerImage => 'localhost:5000/proseo-sample-wrapper:0.7.3'
     },
     {
         processorName => 'PTML2', 
@@ -172,7 +172,7 @@ my @processors = (
         tasks => [ 
            { taskName => 'ptm_l2', taskVersion => '0.1.0' }
         ],
-        dockerImage => 'localhost:5000/proseo-sample-wrapper:0.7.1'
+        dockerImage => 'localhost:5000/proseo-sample-wrapper:0.7.3'
     },
     {
         processorName => 'PTML3', 
@@ -181,7 +181,7 @@ my @processors = (
         tasks => [ 
            { taskName => 'ptm_l3', taskVersion => '0.1.0' }
         ],
-        dockerImage => 'localhost:5000/proseo-sample-wrapper:0.7.1'
+        dockerImage => 'localhost:5000/proseo-sample-wrapper:0.7.3'
     }
 );
 my @configurations = (
