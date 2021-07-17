8fc25ffa52d6345923cde8f5d67376f092c32576
Thomas Bassler <thomas.bassler@drbassler.de>
2021-07-17 20:00:55 +0200

    Revert "Planner: Delete empty product shells for failed job steps, too"
    This reverts commit 2e4e6df92430290ec98a62abea5c58cb2aa5aeb8.

--- planner/src/main/java/de/dlr/proseo/planner/kubernetes/KubeJob.java
+++ planner/src/main/java/de/dlr/proseo/planner/kubernetes/KubeJob.java
@@ -544,11 +544,12 @@ public class KubeJob {
 								for (V1JobCondition jc : jobCondList) {
 									if ((jc.getType().equalsIgnoreCase("complete") || jc.getType().equalsIgnoreCase("completed")) && jc.getStatus().equalsIgnoreCase("true")) {
 										js.get().setJobStepState(JobStepState.COMPLETED);
+										UtilService.getJobStepUtil().checkCreatedProducts(js.get());
+										js.get().incrementVersion();	
 									} else if (jc.getType().equalsIgnoreCase("failed") || jc.getType().equalsIgnoreCase("failure")) {
 										js.get().setJobStepState(JobStepState.FAILED);	
+										js.get().incrementVersion();
 									}
-									UtilService.getJobStepUtil().checkCreatedProducts(js.get());
-									js.get().incrementVersion();	
 								}
 							}
 						}
@@ -675,7 +676,6 @@ public class KubeJob {
 												js.get().setJobStepState(JobStepState.RUNNING);
 											}
 											js.get().setJobStepState(JobStepState.FAILED);	
-											UtilService.getJobStepUtil().checkCreatedProducts(js.get());
 											js.get().incrementVersion();	
 											if (cd == null) {
 												cd = jc.getLastProbeTime();
