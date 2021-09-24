be60cbf357f1f9bb8b74f8263f93ed4abd6b9338
Thomas Bassler <thomas.bassler@drbassler.de>
2021-09-24 17:13:24 +0200

    Logging to file added

--- deploy/proseo-images/push_images.sh
+++ deploy/proseo-images/push_images.sh
@@ -21,16 +21,12 @@ else
   exit 1
 fi 
 
-read -p "Are you sure? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    cd proseo-components
-    for component in proseo-*; do
-      cd $component
-      TAGGED_IMAGENAME=$(cat Dockerfile | grep FROM | awk '{gsub("localhost:5000/",""); split($0,a," "); print a[2]}')-$TAG_SUFFIX
-      docker push $1/$TAGGED_IMAGENAME
-      cd ..
-    done
-fi
+cd proseo-components
+for component in proseo-*; do
+  cd $component
+  TAGGED_IMAGENAME=$(cat Dockerfile | grep FROM | awk '{gsub("localhost:5000/",""); split($0,a," "); print a[2]}')-$TAG_SUFFIX
+  docker push $1/$TAGGED_IMAGENAME
+  cd ..
+done
 
 
