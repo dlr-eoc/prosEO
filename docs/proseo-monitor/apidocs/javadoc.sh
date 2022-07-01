81935841bb9de5e665af2987d54dab62b3ca11ab
Thomas Bassler <thomas.bassler@drbassler.de>
2022-07-01 16:02:14 +0200

    Rebuild of Javadocs and API-Docs, new CLI syntax doc

--- docs/proseo-monitor/apidocs/javadoc.sh
+++ docs/proseo-monitor/apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/javadoc @options @packages
+/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home/bin/javadoc @options @packages
