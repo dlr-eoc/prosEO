ab2606a66734825fd4d7b20e7348d60a1995000b
DenChaykovskiy <den.chaykovskiy@gmail.com>
2022-08-18 17:43:27 +0200

    storage-mgr: s3 file download to cache as temp and rename it

--- storage-mgr/src/main/java/de/dlr/proseo/storagemgr/version2/s3/S3AtomicFileDownloader.java
+++ storage-mgr/src/main/java/de/dlr/proseo/storagemgr/version2/s3/S3AtomicFileDownloader.java
@@ -24,54 +24,57 @@ import software.amazon.awssdk.services.s3.model.GetObjectResponse;
  *
  */
 public class S3AtomicFileDownloader implements AtomicCommand {
-	
+
 	/** Info */
 	private static final String INFO = "S3 ATOMIC File Downloader";
-	
+
 	/** Completed Info */
 	private static final String COMPLETED = "DOWNLOADED";
 
 	/** Logger for this class */
 	private static Logger logger = LoggerFactory.getLogger(S3AtomicFileDownloader.class);
-	
+
+	/** Prefix for temporary files */
+	private static final String TEMPORARY_PREFIX = "temporary-";
+
 	/** source file */
-	private String sourceFile; 
-	
+	private String sourceFile;
+
 	/** target file or dir */
-	private String targetFileOrDir; 
-	
+	private String targetFileOrDir;
+
 	/** S3 Client */
 	private S3Client s3Client;
 
 	/** Bucket */
 	private String bucket;
-	
+
 	/**
 	 * Constructor
 	 * 
-	 * @param s3Client s3 client
-	 * @param bucket bucket
-	 * @param sourceFile sourceFile
+	 * @param s3Client        s3 client
+	 * @param bucket          bucket
+	 * @param sourceFile      sourceFile
 	 * @param targetFileOrDir target file or directory
 	 */
 	public S3AtomicFileDownloader(S3Client s3Client, String bucket, String sourceFile, String targetFileOrDir) {
-		
-		this.s3Client = s3Client; 
-		this.bucket = bucket; 
-		this.sourceFile = sourceFile; 
-		this.targetFileOrDir = targetFileOrDir;  
+
+		this.s3Client = s3Client;
+		this.bucket = bucket;
+		this.sourceFile = sourceFile;
+		this.targetFileOrDir = targetFileOrDir;
 	}
-	
+
 	/**
-	 * Executes download of the file to s3 
+	 * Executes download of the file to s3
 	 * 
-	 * @return downloaded file name 
+	 * @return downloaded file name
 	 */
 	public String execute() throws IOException {
 
 		if (logger.isTraceEnabled())
 			logger.trace(">>> execute() - downloadFile({},{})", sourceFile, targetFileOrDir);
-		
+
 		String sourceS3File = new PathConverter(sourceFile).posixToS3Path().convertToSlash().getPath();
 		String targetPosixFile = targetFileOrDir;
 
@@ -86,18 +89,27 @@ public class S3AtomicFileDownloader implements AtomicCommand {
 		ResponseInputStream<GetObjectResponse> response = s3Client.getObject(request);
 		BufferedOutputStream outputStream;
 
+		String temporaryTargetPosixFile = getTemporaryFilePath(targetPosixFile);
 		try {
-			outputStream = new BufferedOutputStream(new FileOutputStream(targetPosixFile));
+
+			outputStream = new BufferedOutputStream(new FileOutputStream(temporaryTargetPosixFile));
 			byte[] buffer = new byte[4096];
 			int bytesRead = -1;
 
+			// download
 			while ((bytesRead = response.read(buffer)) != -1) {
 				outputStream.write(buffer, 0, bytesRead);
 			}
 
 			response.close();
 			outputStream.close();
-			
+
+			// rename temporary file if the file downloaded successfully to cache 
+			if (!renameFile(temporaryTargetPosixFile, targetPosixFile)) {
+				throw new IOException("Cannot rename file after download to cache " + temporaryTargetPosixFile + " to "
+						+ targetPosixFile);
+			}
+
 			if (logger.isTraceEnabled())
 				logger.trace(">>>>> " + getCompletedInfo() + " - " + targetPosixFile);
 
@@ -105,25 +117,26 @@ public class S3AtomicFileDownloader implements AtomicCommand {
 
 		} catch (IOException e) {
 			logger.warn(e.getMessage());
+			new File(temporaryTargetPosixFile).delete();
 			throw e;
 		}
 	}
-	
+
 	/**
 	 * Gets Information about atomic command (mostly for logs)
 	 * 
 	 * @return Information about atomic command
 	 */
-	public String getInfo() {	
+	public String getInfo() {
 		return INFO;
 	}
-	
+
 	/**
 	 * Gets Information about completed atomic command (mostly for logs)
 	 * 
 	 * @return Information about completed atomic command
 	 */
-	public String getCompletedInfo() {	
+	public String getCompletedInfo() {
 		return INFO + ": " + COMPLETED;
 	}
 
@@ -135,5 +148,36 @@ public class S3AtomicFileDownloader implements AtomicCommand {
 	 */
 	private String getFileName(String path) {
 		return new File(path).getName();
+	}
+
+	/**
+	 * Gets temporary file path
+	 * 
+	 * @param path path
+	 * @return temporary file path
+	 */
+	private String getTemporaryFilePath(String path) {
+
+		String filename = TEMPORARY_PREFIX + new File(path).getName();
+
+		return new PathConverter(new File(path).getParent(), filename).getPath();
+	}
+
+	/**
+	 * Renames file
+	 * 
+	 * @param oldPath old path
+	 * @param newPath new path
+	 * @return true if the file has been renamed successfully
+	 */
+	private boolean renameFile(String oldName, String newName) {
+
+		File oldFile = new File(oldName);
+		File newFile = new File(newName);
+
+		if (newFile.exists())
+			newFile.delete();
+
+		return oldFile.renameTo(newFile);
 	}
 }
