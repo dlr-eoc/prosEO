af1d9a887124762b7ed3723141a8f7d47e09289c
Thomas Bassler <thomas.bassler@drbassler.de>
2023-03-16 12:11:02 +0100

    prosEO 0.9.4 documentation updated

--- docs/proseo-api/proseo-base-monitor/apidocs/package-search-index.js
+++ docs/proseo-api/proseo-base-monitor/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.dlr.proseo.api.basemon"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.dlr.proseo.api.basemon"}];updateSearchResults();
