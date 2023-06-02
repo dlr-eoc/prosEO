6c6a1419b7ccc192e328a324944d37f53238a79d
DenChaykovskiy <den.chaykovskiy@gmail.com>
2023-06-02 14:48:22 +0200

    archive: changed range of product archive manager messages

--- logging/src/main/java/de/dlr/proseo/logging/messages/ProductArchiveMgrMessage.java
+++ logging/src/main/java/de/dlr/proseo/logging/messages/ProductArchiveMgrMessage.java
@@ -16,22 +16,22 @@ import org.slf4j.event.Level;
  */
 public enum ProductArchiveMgrMessage implements ProseoMessage {
 	
-	 DELETION_UNSUCCESSFUL		(5600, Level.ERROR, false, "Product Archive deletion unsuccessful for ID {0}", ""),
-	 DUPLICATED_ARCHIVE			(5601, Level.ERROR, false, "Product Archive {0} exists already", ""),
-	 ARCHIVE_CREATED			(5602, Level.INFO,  true,  "Product Archive with identifier {0} created", ""),
-	 ARCHIVE_DELETED			(5603, Level.INFO,  true,  "Product Archive with id {0} deleted", ""),	 
-	 ARCHIVE_HAS_PRODUCTS		(5604, Level.ERROR, false, "Cannot delete Product Archive {0} due to existing products", ""),
-	 ARCHIVE_ID_MISSING			(5605, Level.ERROR, false, "Product Archive ID not set", ""),
-	 ARCHIVE_LIST_EMPTY			(5606, Level.ERROR, false, "No Product Archives found for search criteria", ""),
-	 ARCHIVE_LIST_RETRIEVED		(5607, Level.INFO,  true,  "Product Archive list of size {0} retrieved for facility ''{1}''", ""),
-	 ARCHIVE_MISSING			(5608, Level.ERROR, false, "Product Archive not set", ""),
-	 ARCHIVE_MODIFIED			(5609, Level.INFO,  true,  "Product Archive with id {0} modified", ""),
-	 ARCHIVE_NOT_FOUND			(5610, Level.ERROR, false, "No Product Archive found for ID {0}", ""),
-	 ARCHIVE_NOT_MODIFIED		(5611, Level.INFO,  true,  "Product Archive with id {0} not modified (no changes)", ""),
-	 ARCHIVE_RETRIEVED			(5612, Level.INFO,  true,  "Product Archive with ID {0} retrieved", ""),
-	 ARCHIVE_TYPE_WRONG			(5613, Level.ERROR, false, "Product Archive Type is wrong: {0}", ""),
-	 DUPLICATED_PRODUCT_CLASS	(5614, Level.ERROR, false, "Duplicated Product Class {0} in Product Archive {1}", ""),
-	 PRODUCT_CLASS_NOT_FOUND    (5614, Level.ERROR, false, "Product Class with code {0} and type {1} not found", ""),
+	 DELETION_UNSUCCESSFUL		(4800, Level.ERROR, false, "Product Archive deletion unsuccessful for ID {0}", ""),
+	 DUPLICATED_ARCHIVE			(4801, Level.ERROR, false, "Product Archive {0} exists already", ""),
+	 ARCHIVE_CREATED			(4802, Level.INFO,  true,  "Product Archive with identifier {0} created", ""),
+	 ARCHIVE_DELETED			(4803, Level.INFO,  true,  "Product Archive with id {0} deleted", ""),	 
+	 ARCHIVE_HAS_PRODUCTS		(4804, Level.ERROR, false, "Cannot delete Product Archive {0} due to existing products", ""),
+	 ARCHIVE_ID_MISSING			(4805, Level.ERROR, false, "Product Archive ID not set", ""),
+	 ARCHIVE_LIST_EMPTY			(4806, Level.ERROR, false, "No Product Archives found for search criteria", ""),
+	 ARCHIVE_LIST_RETRIEVED		(4807, Level.INFO,  true,  "Product Archive list of size {0} retrieved for facility ''{1}''", ""),
+	 ARCHIVE_MISSING			(4808, Level.ERROR, false, "Product Archive not set", ""),
+	 ARCHIVE_MODIFIED			(4809, Level.INFO,  true,  "Product Archive with id {0} modified", ""),
+	 ARCHIVE_NOT_FOUND			(4810, Level.ERROR, false, "No Product Archive found for ID {0}", ""),
+	 ARCHIVE_NOT_MODIFIED		(4811, Level.INFO,  true,  "Product Archive with id {0} not modified (no changes)", ""),
+	 ARCHIVE_RETRIEVED			(4812, Level.INFO,  true,  "Product Archive with ID {0} retrieved", ""),
+	 ARCHIVE_TYPE_WRONG			(4813, Level.ERROR, false, "Product Archive Type is wrong: {0}", ""),
+	 DUPLICATED_PRODUCT_CLASS	(4814, Level.ERROR, false, "Duplicated Product Class {0} in Product Archive {1}", ""),
+	 PRODUCT_CLASS_NOT_FOUND    (4815, Level.ERROR, false, "Product Class with code {0} and type {1} not found", ""),
 	 
 	;
 
