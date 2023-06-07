89a1c4b6f1f9bf7c2866246e1e27e3fd443df3ef
DenChaykovskiy <den.chaykovskiy@gmail.com>
2023-06-07 09:38:31 +0200

    archive: added findArchivesByCode as search criteria in repository

--- model/src/main/java/de/dlr/proseo/model/dao/ProductArchiveRepository.java
+++ model/src/main/java/de/dlr/proseo/model/dao/ProductArchiveRepository.java
@@ -33,12 +33,12 @@ public interface ProductArchiveRepository extends JpaRepository<ProductArchive, 
 	
 	
 	/**
-	 * Get the product archive with the given name
+	 * Get the product archives with the given code
 	 * 
 	 * @param archiveName the name of the productArchive
-	 * @return product archive identified by the name
+	 * @return product archives identified by the code
 	 */
-	@Query("select pa from ProductArchive pa where UPPER(pa.name) = UPPER(?1)")
-	public List<ProductArchive> findByName(String archiveName);
+	@Query("select pa from ProductArchive pa where UPPER(pa.code) = UPPER(?1)")
+	public List<ProductArchive> findArchivesByCode(String archivecode);
 
 }
