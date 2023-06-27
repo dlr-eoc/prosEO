df563b893095b60df10eb56a4a389abd7a587f78
Katharina Bassler <katharina.bassler@drbassler.de>
2023-06-27 10:20:23 +0200

    base-wrapper: javadocs comments extended

---
+++ base-wrapper/src/main/java/de/dlr/proseo/basewrap/rest/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * package-info.java
+ */
+/**
+ * Classes for the base wrapper's REST connections.
+ * 
+ * @author Hubert Asamer
+ * @author Dr. Thomas Bassler
+ */
+package de.dlr.proseo.basewrap.rest;
