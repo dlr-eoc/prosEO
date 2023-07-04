5e0f7b63a5f5a0fe6598db3584bce8b013d21daa
Katharina Bassler <katharina.bassler@drbassler.de>
2023-07-04 16:19:37 +0200

    ui-cli: formatted

--- ui/gui/src/main/java/de/dlr/proseo/ui/gui/GraphicalUserInterface.java
+++ ui/gui/src/main/java/de/dlr/proseo/ui/gui/GraphicalUserInterface.java
@@ -1,34 +1,30 @@
 /**
  * GraphicalUserInterface.java
- * 
+ *
  * (C) 2019 Dr. Bassler & Co. Managementberatung GmbH
  */
-
 package de.dlr.proseo.ui.gui;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+
 /**
  * prosEO Graphical User Interface application
- * 
+ *
  * @author Dr. Thomas Bassler
- * 
  */
 @SpringBootApplication
-////@Configuration
-////@EnableAutoConfiguration
-////@EnableConfigurationProperties
-@ComponentScan(basePackages={"de.dlr.proseo.ui"})
+//@Configuration
+//@EnableAutoConfiguration
+//@EnableConfigurationProperties
+@ComponentScan(basePackages = { "de.dlr.proseo.ui" })
 //@Configuration
 //@EnableAutoConfiguration
 @EnableConfigurationProperties
-
-public class GraphicalUserInterface  {
-
+public class GraphicalUserInterface {
 	public static void main(String[] args) throws Exception {
-		
 		SpringApplication.run(GraphicalUserInterface.class, args);
 	}
-	
 }
