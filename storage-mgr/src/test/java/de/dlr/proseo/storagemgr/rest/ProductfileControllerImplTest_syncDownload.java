ed9d8287a3992ba0e6a17efed705ae3976cf49b4
DenChaykovskiy <den.chaykovskiy@gmail.com>
2024-02-14 14:34:16 +0100

    storage-mgr: added thread processing to the sync download testing

--- storage-mgr/src/test/java/de/dlr/proseo/storagemgr/rest/ProductfileControllerImplTest_syncDownload.java
+++ storage-mgr/src/test/java/de/dlr/proseo/storagemgr/rest/ProductfileControllerImplTest_syncDownload.java
@@ -47,13 +47,13 @@ public class ProductfileControllerImplTest_syncDownload {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private BaseStorageTestUtils storageTestUtils;
-	
- 	@Autowired
+
+	@Autowired
 	private StorageProvider storageProvider;
-	
+
 	@Rule
 	public TestName testName = new TestName();
 
@@ -61,100 +61,140 @@ public class ProductfileControllerImplTest_syncDownload {
 
 	@Test
 	public void testDownload_posix() throws Exception {
-		
-		StorageType storageType = StorageType.POSIX; 
+
+		StorageType storageType = StorageType.POSIX;
 		storageProvider.setStorage(storageType);
 
 		syncDownload("Posix");
-		
+
 		StorageType realStorageType = storageProvider.getStorage().getStorageType();
 		assertTrue("Expected: SM POSIX, " + " Exists: " + realStorageType, storageType == realStorageType);
 	}
 	
 	@Test
 	public void testDownload_S3() throws Exception {
-		
-		StorageType storageType = StorageType.S3; 
+
+		StorageType storageType = StorageType.S3;
 		storageProvider.setStorage(storageType);
 
 		syncDownload("S3");
-		
+
 		StorageType realStorageType = storageProvider.getStorage().getStorageType();
 		assertTrue("Expected: SM S3, " + " Exists: " + realStorageType, storageType == realStorageType);
 	}
-		
+
 	/**
 	 * DOWNLOAD Storage -> Cache (getRestFileInfoByPathInfo)
 	 *
-	 * takes filename from path and productid from parameter, ignores the rest of the path
+	 * takes filename from path and productid from parameter, ignores the rest of
+	 * the path
 	 * 
-	 * INPUT 
+	 * INPUT
 	 * 
-	 * absolutePath  	 
-	 * s3://<bucket>/<relativePath>        // no storage path in s3
-	 * /<storagePath>/<relativePath>       // no bucket in posix currently
+	 * absolutePath s3://<bucket>/<relativePath> // no storage path in s3
+	 * /<storagePath>/<relativePath> // no bucket in posix currently
 	 * 
-	 * OUTPUT 
+	 * OUTPUT
 	 * 
-	 * Posix only (cache):  /<cachePath>/<relativePath>
+	 * Posix only (cache): /<cachePath>/<relativePath>
 	 */
 	private void syncDownload(String testID) throws Exception {
-		
+
 		TestUtils.printMethodName(this, testName);
-		
+
 		// create file in source
-		// upload to storage 
-		// call http-download 
+		// upload to storage
+		// call http-download
 		// check results (download in cache)
-		
+
 		String relativePath = "product/productFileDownload" + testID + ".txt";
 		relativePath = new PathConverter(relativePath).getPath();
-	
-		// create file in source 
+
+		// create file in source
 		long fileSizeInBytes = 100L * 1024 * 1024; // 100 MB
 		String absoluteSourcePath = storageTestUtils.createSourceFile(relativePath, fileSizeInBytes);
-		
+
 		// upload file to storage from source
 		StorageFile sourceFile = storageProvider.getSourceFile(relativePath);
 		StorageFile storageFile = storageProvider.getStorageFile(relativePath);
 		storageProvider.getStorage().upload(sourceFile, storageFile);
-		
+
 		// show storage files
 		BaseStorageTestUtils.printStorageFiles("Before http-call", storageProvider.getStorage());
 
-		// rest-download file from storage to cache
-		String absoluteStoragePath = storageProvider.getStorage().getAbsolutePath(relativePath);
-		System.out.println("Http-download call path (absolute storage path):" + absoluteStoragePath);
-		
-		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get(REQUEST_STRING)
-				.param("pathInfo", absoluteStoragePath);
-		MvcResult mvcResult = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
-		
-		// show results of http-download
-		TestUtils.printMvcResult(REQUEST_STRING, mvcResult); 
+		DownloadThread thread1 = new DownloadThread(relativePath);
+		thread1.start();
 
-		storageTestUtils.printCache();
-		
-		// check real with expected absolute cache path 
-		String expectedAbsoluteCachePath = new PathConverter(storageProvider.getCachePath(), relativePath).getPath();
-		
-		String json = mvcResult.getResponse().getContentAsString();
-		RestFileInfo result = new ObjectMapper().readValue(json, RestFileInfo.class);
-		String realAbsoluteCachePath = result.getFilePath();
-		
-		System.out.println("Real cache path:     " + realAbsoluteCachePath);
-		System.out.println("Expected cache path: " + expectedAbsoluteCachePath);
-		
-		realAbsoluteCachePath = new PathConverter(realAbsoluteCachePath).normalizeWindowsPath().getPath();
-		assertTrue("Real cache path: " + realAbsoluteCachePath + " expected cache path: " + expectedAbsoluteCachePath, 
-				realAbsoluteCachePath.equals(expectedAbsoluteCachePath));
-		
-		assertTrue("Downloaded file from storage to cache does not exist: " + realAbsoluteCachePath, 
-				new File(realAbsoluteCachePath).exists());
-		
+		DownloadThread thread2 = new DownloadThread(relativePath);
+		thread2.start();
+
 		// delete files with empty folders
-		new FileUtils(absoluteSourcePath).deleteFile(); // source
-		new FileUtils(expectedAbsoluteCachePath).deleteFile(); // cache
-		storageProvider.getStorage().deleteFile(storageFile); // in storage
+
+		String cacheFile = new PathConverter(storageProvider.getCachePath(), relativePath).getPath();
+
+		if (new File(absoluteSourcePath).exists()) {
+			new FileUtils(absoluteSourcePath).deleteFile(); // source
+		}
+		
+		if (new File(cacheFile).exists()) {
+			new FileUtils(cacheFile).deleteFile(); // cache
+		}
+		
+		if (new File(storageFile.getFullPath()).exists()) {
+			storageProvider.getStorage().deleteFile(storageFile); // in storage
+		}
+	}
+
+	private class DownloadThread extends Thread {
+
+		private String relativePath;
+
+		public DownloadThread(String relativePath) {
+
+			this.relativePath = relativePath;
+		}
+
+		public void run() {
+			
+			System.out.println("Thread name:" + Thread.currentThread().getName());
+
+			// rest-download file from storage to cache
+			String absoluteStoragePath = storageProvider.getStorage().getAbsolutePath(relativePath);
+			System.out.println("Http-download call path (absolute storage path):" + absoluteStoragePath);
+
+			MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get(REQUEST_STRING).param("pathInfo",
+					absoluteStoragePath);
+			MvcResult mvcResult;
+
+			try {
+				mvcResult = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
+
+				// show results of http-download
+				TestUtils.printMvcResult(REQUEST_STRING, mvcResult);
+
+				storageTestUtils.printCache();
+
+				// check real with expected absolute cache path
+				String expectedAbsoluteCachePath = new PathConverter(storageProvider.getCachePath(), relativePath)
+						.getPath();
+
+				String json = mvcResult.getResponse().getContentAsString();
+				RestFileInfo result = new ObjectMapper().readValue(json, RestFileInfo.class);
+				String realAbsoluteCachePath = result.getFilePath();
+
+				System.out.println("Real cache path:     " + realAbsoluteCachePath);
+				System.out.println("Expected cache path: " + expectedAbsoluteCachePath);
+
+				realAbsoluteCachePath = new PathConverter(realAbsoluteCachePath).normalizeWindowsPath().getPath();
+				assertTrue("Real cache path: " + realAbsoluteCachePath + " expected cache path: "
+						+ expectedAbsoluteCachePath, realAbsoluteCachePath.equals(expectedAbsoluteCachePath));
+
+				assertTrue("Downloaded file from storage to cache does not exist: " + realAbsoluteCachePath,
+						new File(realAbsoluteCachePath).exists());
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
