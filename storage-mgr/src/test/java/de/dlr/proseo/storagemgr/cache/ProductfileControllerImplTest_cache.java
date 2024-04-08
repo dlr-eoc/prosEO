c886baf22286b59d3bff6e6129f3bda57e949e87
DenChaykovskiy <den.chaykovskiy@gmail.com>
2024-04-08 13:01:55 +0200

    storage-mgr: adjusted to spring boot 2.7.18 - adapted LocalServerPort

--- storage-mgr/src/test/java/de/dlr/proseo/storagemgr/cache/ProductfileControllerImplTest_cache.java
+++ storage-mgr/src/test/java/de/dlr/proseo/storagemgr/cache/ProductfileControllerImplTest_cache.java
@@ -18,7 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
