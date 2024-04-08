7ce18b251685c9969811d26d3a84be3440c15aeb
Katharina Bassler <katharina.bassler@drbassler.de>
2024-04-08 12:19:46 +0200

    planner: package-info.java added

---
+++ planner/src/main/java/de/dlr/proseo/planner/dispatcher/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * package-info.java
+ * 
+ * (C) 2019 Dr. Bassler & Co. Managementberatung GmbH
+ */
+/**
+ * Classes for dispatching orders, jobs and job steps with the prosEO Production Planner component
+ * 
+ * @author Dr. Thomas Bassler
+ *
+ */
+package de.dlr.proseo.planner.dispatcher;
---
+++ planner/src/main/java/de/dlr/proseo/planner/kubernetes/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * package-info.java
+ * 
+ * (C) 2019 Dr. Bassler & Co. Managementberatung GmbH
+ */
+/**
+ * Kubernetes utility classes for the prosEO Production Planner component
+ * 
+ * @author Dr. Thomas Bassler
+ *
+ */
+package de.dlr.proseo.planner.kubernetes;
