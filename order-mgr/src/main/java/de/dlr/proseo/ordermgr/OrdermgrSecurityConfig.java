55369224e809667126aed051041e1f6df27fcd00
DenChaykovskiy <den.chaykovskiy@gmail.com>
2024-04-09 12:06:19 +0200

    order-mgr: removed authentication builder include from security config

--- order-mgr/src/main/java/de/dlr/proseo/ordermgr/OrdermgrSecurityConfig.java
+++ order-mgr/src/main/java/de/dlr/proseo/ordermgr/OrdermgrSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
