263978007438385ec9e43c5bb6a6b2f671eda773
DenChaykovskiy <den.chaykovskiy@gmail.com>
2024-04-26 11:35:23 +0200

    user-mgr: removed unnecessary repository annotations

--- user-mgr/src/main/java/de/dlr/proseo/usermgr/dao/GroupRepository.java
+++ user-mgr/src/main/java/de/dlr/proseo/usermgr/dao/GroupRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import de.dlr.proseo.usermgr.model.Group;
 
@@ -18,7 +17,6 @@ import de.dlr.proseo.usermgr.model.Group;
  *
  * @author Dr. Thomas Bassler *
  */
-@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
 	/**
