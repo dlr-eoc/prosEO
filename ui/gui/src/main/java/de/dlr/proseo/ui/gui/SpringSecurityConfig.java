99d93d7cb1683f943b1b4d78b6767f11144f214d
Katharina Bassler <katharina.bassler@drbassler.de>
2024-05-16 11:13:31 +0200

    ui-gui: authentication manager generation changed

--- ui/gui/src/main/java/de/dlr/proseo/ui/gui/SpringSecurityConfig.java
+++ ui/gui/src/main/java/de/dlr/proseo/ui/gui/SpringSecurityConfig.java
@@ -5,10 +5,13 @@
  */
 package de.dlr.proseo.ui.gui;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,45 +30,47 @@ public class SpringSecurityConfig {
 	@Autowired
 	private GUIAuthenticationProvider authenticationProvider;
 
-    /**
-     * Configures the HTTP security settings, including the authentication filter, URL permissions, login and logout pages, and CSRF
-     * protection.
-     *
-     * @param http the HttpSecurity object to be configured
-     * @throws Exception if an error occurs during configuration
-     */
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
+	/**
+	 * Configures the HTTP security settings, including the authentication filter, URL permissions, login and logout pages, and CSRF
+	 * protection.
+	 *
+	 * @param http the HttpSecurity object to be configured
+	 * @throws Exception if an error occurs during configuration
+	 */
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
 		// Create an instance of the custom authentication filter
 		SpringAuthenticationFilter authenticationFilter = new SpringAuthenticationFilter();
-		authenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
+		authenticationFilter.setAuthenticationManager(authenticationManagerBean());
 		authenticationFilter.setFilterProcessesUrl("/customlogin");
 
 		// Configure HTTP security
-        http.addFilter(authenticationFilter)
-        		.authenticationProvider(authenticationProvider)
-                .authorizeRequests(requests -> requests
-                        .antMatchers("/resources/**")
-                        .permitAll()
-                        .antMatchers("/background.jpg")
-                        .permitAll()
-                        .antMatchers("/**/actuator/health")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .formLogin(login -> login
-                        .loginPage("/customlogin")
-                        .failureUrl("/customlogin?error")
-                        .permitAll())
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/customlogin?logout")
-                        .permitAll())
-                .csrf(csrf -> csrf
-                        .disable());
-		
+		http.addFilter(authenticationFilter)
+			.authenticationProvider(authenticationProvider)
+			.authorizeRequests(requests -> requests.antMatchers("/resources/**")
+				.permitAll()
+				.antMatchers("/background.jpg")
+				.permitAll()
+				.antMatchers("/**/actuator/health")
+				.permitAll()
+				.anyRequest()
+				.authenticated())
+			.formLogin(login -> login.loginPage("/customlogin").failureUrl("/customlogin?error").permitAll())
+			.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/customlogin?logout").permitAll())
+			.csrf(csrf -> csrf.disable());
+
 		return http.build();
 	}
 
+	/**
+	 * Provides the authentication manager, which manages authentication attempts within the application.
+	 *
+	 * @return An instance of AuthenticationManager configured with the GUIAuthenticationProvider.
+	 * @throws Exception if an error occurs during the instantiation of AuthenticationManager.
+	 */
+	@Bean
+	AuthenticationManager authenticationManagerBean() throws Exception {
+		return new ProviderManager(Collections.singletonList(authenticationProvider));
+	}
 }
