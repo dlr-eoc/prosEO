bed02635633427ffc5969f7854ce70383a487ee4
Katharina Bassler <katharina.bassler@drbassler.de>
2026-03-24 09:24:10 +0100

    deploy-cdse: log scraping automated with promtail and loki
    - promtail (log shipping) installed on brain, loghost, nfsserver
    - loki (log searching and alerting) installed on loghost
    - postfix configured for actual script-based alert delivery
    - grafana configuration updated accordingly
    - loghost directory /opt/proseo renamed to /opt/prosEO
    - convenience scripts added

---
+++ deploy/loghost/prepare_monitoring/files/alerting.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+MAIL_TO="somereceiver@example.de otherreceiver@example.de"
+SUBJECT_PREFIX="[CDSE CloudFerro]"
+
+EXCLUDE_PATTERNS=(
+  "CleanupProducts"
+  "SomeOtherNoise"
+)
+
+content=$(curl --max-time 60 --fail --show-error -sG "http://localhost:3100/loki/api/v1/query_range" \
+  -H "X-Scope-OrgID: 1" \
+  --data-urlencode 'query={job="proseo"} |= "ERROR"' \
+  --data-urlencode "start=$(date -d '30 minutes ago' +%s%N)" \
+  --data-urlencode "end=$(date +%s%N)" \
+  --data-urlencode "limit=100" \
+  --data-urlencode "direction=backward" \
+| jq -r '
+  .data.result[] as $r
+  | $r.values[]
+  | "[\($r.stream.filename)] \(. [1])"
+')
+
+# apply filters
+for p in "${EXCLUDE_PATTERNS[@]}"; do
+  content=$(grep -vi "$p" <<< "$content")
+done
+
+count=$(echo "$content" | grep -c .)
+
+if [ "$count" -gt 0 ]; then
+  {
+    echo "Note that at most 100 log lines are shown. Please refer to the source logs for more context."
+    echo
+    echo "$content"
+  } | mail -s "$SUBJECT_PREFIX prosEO errors (last 30m, $count lines)" "$MAIL_TO"
+fi
