74a9666996198c3fbc135d675a3697257eacbeb7
melchinger <melchinger@prophos.de>
2026-08-07 08:46:09 +0200

    Processor-Mgr: Unit tests updated to JUnit6, some unnecessary imports removed.

--- processor-mgr/src/test/java/de/dlr/proseo/procmgr/rest/ConfiguredProcessorManagerTest.java
+++ processor-mgr/src/test/java/de/dlr/proseo/procmgr/rest/ConfiguredProcessorManagerTest.java
@@ -11,9 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.util.UUID;
--- processor-mgr/src/test/java/de/dlr/proseo/procmgr/rest/ProcessorControllerTest.java
+++ processor-mgr/src/test/java/de/dlr/proseo/procmgr/rest/ProcessorControllerTest.java
@@ -12,10 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
